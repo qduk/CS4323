049f32c7312c6f18d00f2e22543fe6ec8b5ee3bf
avirishi <70116669+avirishi@users.noreply.github.com>
2021-03-28 23:13:01 -0500

    Add files via upload

--- structure.c
+++ structure.c
@@ -5,6 +5,7 @@
 #include <fcntl.h> 
 #include <sys/stat.h> 
 #include <sys/types.h> 
+#include <stdlib.h>
 
 
 /**********Structure**********/
@@ -16,48 +17,71 @@ struct User{
 
 /**********UserInput**********/
 int structure(){
-  
+
   struct User *userPtr, user;
   userPtr = &user;
-  char option = 'y';
-  char ch;
-  char *arr;
-
   int fd;
-  // FIFO file path
+  // file path
   char * myfifo = "myfifo.txt";
-  // Creating file FIFO
+  // Creating the file
   mkfifo(myfifo, 0644);
+  char arr, arr1, arr2;
+  char ch, temp;
+  // Open file fo write only
+  fd = open(myfifo, O_WRONLY);
 
-
-
-  if (option == 'y'){
-    do{
+  printf("click enter to continue\n");
+  while(1){
+      scanf("%c",&temp); // temp statement to clear buffer
+      // Take user UserInput and write to file
       printf("Enter Employee Name: ");
-      scanf("%s", userPtr->employeeName);
+      scanf("%[^\n]", userPtr->employeeName);
 
       printf("Enter Job Title: ");
-      scanf("%s", userPtr->jobTitle);
+      scanf("%c",&temp); // temp statement to clear buffer
+      scanf("%[^\n]", userPtr->jobTitle);
 
       printf("Enter Status: ");
-      scanf("%s", userPtr->status);
+      scanf("%c",&temp); // temp statement to clear buffer
+      scanf("%[^\n]", userPtr->status);
 
-      arr = userPtr->employeeName;
-      write(fd, arr, strlen(arr)+1);
+      char *space = ",";   
+      char *arr = userPtr->employeeName;
+      char *arr1 = userPtr->jobTitle;
+      char *arr2 = userPtr->status;
+     
+      // Concanating 3 strings together
+      unsigned int const sz1  = strlen(arr);
+      unsigned int const sz2  = strlen(arr1);
+      unsigned int const sz3  = strlen(arr2);
+      unsigned int const szSpace  = strlen(space);
+
+      char *concat = (char*)malloc(sz1+sz2+sz3+3);
+
+      memcpy( concat, arr  , sz1 );
+      memcpy( concat+sz1, space , szSpace);
+      memcpy( concat+sz1+szSpace, arr1 , sz2 );
+      memcpy( concat+sz1+szSpace+sz2, space , szSpace);
+      memcpy( concat+sz1+szSpace+sz2+szSpace, arr2 , sz3 );
+     memcpy( concat+sz1+szSpace+sz2+szSpace+sz3, space , szSpace);
+    
+    concat[sz1+ szSpace + sz2 + szSpace + sz3 +szSpace] = '\0';
+
+    write(fd, concat, strlen(concat));
+
+    //printf("%s\n", concat);
 
       printf("Do you want to continue? y/n: ");
       scanf("%s", &ch);
-    }
-    while(ch == 'y');
+
+      if(ch == 'y'){
+        continue;
+      }
+      // End program if user input is 'n'
+      else if(ch == 'n'){
+        close(fd);
+        return 0;
+      }
   }
-    fd = open(myfifo, O_WRONLY);
-    close(fd);
-
-    //fd = open(myfifo, O_RDONLY);
-    //read(fd, arr1, sizeof(arr1) != strlen(arr1));
-
-    //printf("User2: %s\n", arr1);
-    //close(fd);
-
   return 0;
 }
